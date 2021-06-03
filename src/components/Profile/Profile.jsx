dea58db71c6468339aade88c1ffabe124f33e3a7
SparkoV1 <79722860+SparkoV1@users.noreply.github.com>
2021-06-03 21:44:40 +0200

    24

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
 import s from "./Profile.module.css";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
-  return (
-    <div className={s.content}>
-      <div>
-        <img
-          src="https://t4.ftcdn.net/jpg/03/14/28/43/360_F_314284363_5BOhmmlrpeTUVtNCw7xbKaUjChtaarBV.jpg"
-          alt=""
-        />
-      </div>
-      <div>ava + description</div>
-      <MyPosts />
-    </div>
-  );
+    return (
+        <div>
+            <ProfileInfo/>
+            <MyPosts/>
+        </div>
+    );
 };
 
 export default Profile;
