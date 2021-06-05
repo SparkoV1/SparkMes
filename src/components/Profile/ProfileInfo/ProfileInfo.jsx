29439e8563f2d2cdcba6bd04a6812c909ace411f
SparkoV1 <79722860+SparkoV1@users.noreply.github.com>
2021-06-05 18:57:38 +0200

    24

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import s from "./ProfileInfo.module.css";
+
+const ProfileInfo = () => {
+    return (
+        <div>
+            <div>
+                <img
+                    src="https://t4.ftcdn.net/jpg/03/14/28/43/360_F_314284363_5BOhmmlrpeTUVtNCw7xbKaUjChtaarBV.jpg"
+                    alt=""
+                />
+            </div>
+            <div className={s.descriptionBlock}>ava + description</div>
+        </div>
+    );
+};
+
+export default ProfileInfo;
