3cce5c84561c0c7d50a8a1c7107651630feb3785
SparkoV1 <79722860+SparkoV1@users.noreply.github.com>
2021-08-30 13:00:13 +0200

    08/30/2021_add_axios

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,11 +2,13 @@ import {combineReducers, createStore} from "redux";
 import profileReducer from "./reducer/profile-reducer";
 import dialogsReducer from "./reducer/dialogs-reducer";
 import sidebarReducer from "./reducer/sidebar-reducer";
+import usersReducer from "./reducer/users-reducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogPage: dialogsReducer,
     sidebar: sidebarReducer,
+    usersPage: usersReducer
 });
 
 let store = createStore(reducers);
