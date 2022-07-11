8cfc4ce826786faff1fb9e4bd48510a933c1e63d
SparkoV1 <79722860+SparkoV1@users.noreply.github.com>
2022-07-11 21:27:52 +0200

    changePageCaunter

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,60 +1,79 @@
 import React from "react";
 import "./Users.scss";
 import userPhoto from "../../assets/images/user.png";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Users = (props) => {
+  // let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+  let pagesCount = 10;
+  let pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
+  }
 
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i)
-    }
-
-    return (
-        <>
-            <div className="pageNumber">
-                {pages.map(p => {
-                    return <span className={props.currentPage === p && "selectedPage"} onClick={() => {
-                        props.onPageChanged(p)
-                    }}>{p}</span>
-                })}
+  return (
+    <>
+      <div className="pageNumber">
+        {pages.map((p) => {
+          return (
+            <span
+              className={props.currentPage === p && "selectedPage"}
+              onClick={() => {
+                props.onPageChanged(p);
+              }}
+            >
+              {p}
+            </span>
+          );
+        })}
+      </div>
+      {props.users.map((u) => (
+        <div key={u.id}>
+          <span>
+            <div>
+              <NavLink to={"/profile/" + u.id}>
+                <img
+                  src={u.photos.small != null ? u.photos.small : userPhoto}
+                  className="userPhoto"
+                  alt={"#"}
+                />
+              </NavLink>
             </div>
-            {
-                props.users.map(u => <div key={u.id}>
-                    <span>
-                        <div>
-                            <NavLink to={'/profile/' + u.id}>
-                                <img src={u.photos.small != null ? u.photos.small : userPhoto} className="userPhoto"
-                                     alt={"#"}/>
-                            </NavLink>
-
-                        </div>
-                        <div>
-                            {u.followed
-                                ? <button onClick={() => {
-                                    props.unfollow(u.id)
-                                }}>Unfollow</button>
-                                : <button onClick={() => {
-                                    props.follow(u.id)
-                                }}>Follow</button>}
-                        </div>
-                    </span>
-                    <span>
-                        <span>
-                            <div>{u.name}</div>
-                            <div>{u.status}</div>
-                        </span>
-                        <span>
-                            <div>{"u.location.country"}</div>
-                            <div>{"u.location.city"}</div>
-                        </span>
-                    </span>
-                </div>)
-            }
-        </>
-    )
-}
+            <div>
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(u.id);
+                  }}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(u.id);
+                  }}
+                >
+                  Follow
+                </button>
+              )}
+            </div>
+          </span>
+          <span>
+            <span>
+              <div>{u.name}</div>
+              <div>{u.status}</div>
+            </span>
+            <span>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
+            </span>
+          </span>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default Users;
